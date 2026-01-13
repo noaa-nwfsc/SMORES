5a0a77b554ba5f193375d22c7dd8f6d52c163a60
Melissa Widas <melissa.widas@noaa.gov>
2026-01-13 09:39:03 -0800

    removed buffer from individual maps

--- R/crop_data_to_aoi.R
+++ R/crop_data_to_aoi.R
@@ -1,5 +1,5 @@
 # Function to crop spatial data to Area of Interest
-crop_data_to_aoi <- function(dataset, aoi_data, buffer_distance = 1000) {
+crop_data_to_aoi <- function(dataset, aoi_data) {
   
   # Skip cropping for Trawl Fisheries data to avoid geometry issues
   if("Score.Trawl_Fisheries" %in% names(dataset)) {
@@ -26,15 +26,8 @@ crop_data_to_aoi <- function(dataset, aoi_data, buffer_distance = 1000) {
       aoi_data <- st_transform(aoi_data, st_crs(dataset))
     }
     
-    # Optional: Buffer AOI slightly to capture edge effects
-    if(buffer_distance > 0) {
-      aoi_buffered <- st_buffer(aoi_data, dist = buffer_distance)
-    } else {
-      aoi_buffered <- aoi_data
-    }
-    
     # Spatial intersection to crop data
-    cropped_data <- st_intersection(dataset, aoi_buffered)
+    cropped_data <- st_intersection(dataset, aoi_data)
     
     
     # Handle edge cases
